bc089566dcecc8a104765982f7784b83f6f3f50a
Paulo Novo <143038290+shoteose@users.noreply.github.com>
2025-05-02 15:49:37 +0100

    NavBar

--- app/src/main/java/com/example/roteiroandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/roteiroandroidapp/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.roteiroandroidapp
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        loadFragment(IndexViagemFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.perfil -> {
+                    loadFragment(IndexPerfilFragment())
+                    true
+                }
+                R.id.viagem -> {
+                    loadFragment(IndexViagemFragment())
+                    true
+                }
+
+                else -> false
+            }
         }
     }
+    private fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
 }

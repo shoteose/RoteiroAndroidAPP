49dd4dd7f24cf55879baf8dca913bfc8c1c7f64b
mchuguinho <hugodinis2001@gmail.com>
2025-05-08 20:57:57 +0100

    Language selector

---
+++ app/src/main/java/com/example/roteiroandroidapp/LocaleHelper.kt
@@ -0,0 +1,17 @@
+package com.example.roteiroandroidapp
+
+import android.content.Context
+import android.content.res.Configuration
+import java.util.*
+
+object LocaleHelper {
+    fun setLocale(context: Context, language: String): Context {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+
+        val config = Configuration(context.resources.configuration)
+        config.setLocale(locale)
+
+        return context.createConfigurationContext(config)
+    }
+}
